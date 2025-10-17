960b512fe9fcc9cdcc2d0fa53347f86cc653ce31
Sanskriti Garg <sanskritii.garg@gmail.com>
2025-10-17 12:00:53 +0530

    Cart and Product Slice

---
+++ frontend/redux/slices/productsSlice.js
@@ -0,0 +1,184 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Async Thunk to fetch Products by Collection and optional Filters
+export const fetchProductsByFilters = createAsyncThunk(
+  'products/fetchByFilters',
+  async ({
+    collection,
+    size,
+    color,
+    gender,
+    minPrice,
+    maxPrice,
+    sortBy,
+    search,
+    category,
+    material,
+    brand,
+    limit,
+  }) => {
+    const query = new URLSearchParams();
+    if (collection) query.append('collection', collection);
+    if (size) query.append('size', size);
+    if (color) query.append('color', color);
+    if (gender) query.append('gender', gender);
+    if (minPrice) query.append('minPrice', minPrice);
+    if (maxPrice) query.append('maxPrice', maxPrice);
+    if (sortBy) query.append('sortBy', sortBy);
+    if (search) query.append('search', search);
+    if (material) query.append('material', material);
+    if (brand) query.append('brand', brand);
+    if (limit) query.append('limit', limit);
+    if (category) query.append('category', category);
+
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/api/products?${query.toString()}`,
+    );
+
+    return response.data;
+  },
+);
+
+// Async thunk to fetch a single product by id
+export const fetchProductDetails = createAsyncThunk('products/fetchProductDetails', async (id) => {
+  const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products/${id}`);
+
+  return response.data;
+});
+
+// Async thunk to update product by id
+export const updateProduct = createAsyncThunk(
+  'products/updateProduct',
+  async ({ id, productData }) => {
+    const response = await axios.put(
+      `${import.meta.env.VITE_BACKEND_URL}/products/api/${id}`,
+      productData,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+        },
+      },
+    );
+
+    return response.data;
+  },
+);
+
+// Async thunk to fetch similar products
+export const fetchSimilarProducts = createAsyncThunk(
+  'products/fetchSimilarProducts',
+  async ({ id }) => {
+    const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/products?${id}`);
+
+    return response.data;
+  },
+);
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: {
+    products: [],
+    selectedProduct: null,
+    similarProducts: [],
+    loading: false,
+    error: null,
+    filters: {
+      category: '',
+      size: '',
+      color: '',
+      gender: '',
+      minPrice: '',
+      maxPrice: '',
+      material: '',
+      sortBy: '',
+      search: '',
+      brand: '',
+      collection: '',
+    },
+  },
+  reducers: {
+    setFilters: (state, action) => {
+      state.filters = { ...state.filters, ...action.payload };
+    },
+    clearFilters: (state) => {
+      state.filters = {
+        category: '',
+        size: '',
+        color: '',
+        gender: '',
+        minPrice: '',
+        maxPrice: '',
+        material: '',
+        sortBy: '',
+        search: '',
+        brand: '',
+        collection: '',
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Handle fetching products by filters
+      .addCase(fetchProductsByFilters.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProductsByFilters.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = Array.isArray(action.payload) ? action.payload : [];
+      })
+      .addCase(fetchProductsByFilters.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      // Handle fetching details of a product
+      .addCase(fetchProductDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.selectedProduct = action.payload;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      //Handle updating product details
+      .addCase(updateProduct.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateProduct.fulfilled, (state, action) => {
+        state.loading = false;
+        const updatedProduct = action.payload;
+
+        const index = state.products.findIndex((product) => product._id === updateProduct._id);
+
+        if (index !== -1) {
+          state.products[index] = updatedProduct;
+        }
+      })
+      .addCase(updateProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      // Handle fetching similar products
+      .addCase(fetchSimilarProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchSimilarProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.similarProducts = Array.isArray(action.payload) ? action.payload : [];
+      })
+      .addCase(fetchSimilarProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setFilters, clearFilters } = productsSlice.actions;
+export default productsSlice.reducer;
--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice.js';
+import productReducer from './slices/productsSlice.js';
+import cartReducer from './slices/cartSlice.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productReducer,
+    cart: cartReducer,
   },
 });
 
